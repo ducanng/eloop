95e721979ff57d198cd0108d4dab0de49bf82872
Weed <phamhuyhoang3004@gmail.com>
2022-11-27 01:33:56 +0700

    fix login , logout

--- home/homeController.js
+++ home/homeController.js
@@ -1,12 +1,4 @@
-const {partner,findPartner,addPartner,removePartner,getPartnerList} = require('../models/partner')
 
-exports.getPartner = async (req,res,next) =>{
-    const partnerList = await getPartnerList()
-    console.log(partnerList.length);
-    partnerList.forEach(element => {
-       console.log(element.name);  
-    });
- 
-    console.log(global.userLogined);
+exports.getHome= async (req,res,next) =>{ 
     res.render('users/home')
 }
